79a73d96a7f9ae3aa09146cffe7833b06e516e76
MaxWoden <kungfu-panda007@mail.ru>
2024-10-15 15:45:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default function App() {
+	return (
+		<div className='relative h-screen'>
+      <h1 className='font-bold text-8xl text-center pt-10 text-orange-500'>404</h1>
+			<img className='absolute bottom-0' src={require('./vincent.gif')} />
+		</div>
+	)
+};
